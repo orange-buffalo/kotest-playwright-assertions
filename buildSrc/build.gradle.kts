a6caf9667efb163782cdc79998a1917e259ad213
orange-buffalo <orange-buffalo@users.noreply.github.com>
2025-06-21 12:34:38 +1000

    build: Upgrading Kotlin used for code generation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    `kotlin-dsl`
+    kotlin("jvm") version "2.1.21"
 }
 
 repositories {
@@ -11,7 +11,7 @@ dependencies {
     implementation("com.squareup:kotlinpoet:1.18.1")
     implementation("io.ktor:ktor-client-core:2.3.12")
     implementation("io.ktor:ktor-client-cio:2.3.12")
-    
+
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.1")
